6e4e50ec7609107a60ab640d1b799dc3b5e711de
KiranAI007 <kiranrkrishnan07@gmail.com>
2024-09-06 16:33:11 +0530

    After some code changes

--- test/test_operations.py
+++ test/test_operations.py
@@ -1,1 +1,12 @@
 from src.math_operation import add, sub
+
+# define the test cases for add
+def test_add():
+    assert add(2,3)==5
+    assert add(-1,1)==0
+
+# define the test cases for subtract
+def test_sub():
+    assert sub(5, 3) == 2
+    assert sub(-2, 5) == -7
+    
